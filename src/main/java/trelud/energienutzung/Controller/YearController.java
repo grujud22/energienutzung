7517ba292d8a50da123c96eb5f9b410b23e3c2d5
trelud22 <trelud22@htl-kaindorf.at>
2026-05-26 16:09:43 +0200

    Updated API names

--- src/main/java/trelud/energienutzung/Controller/YearController.java
+++ src/main/java/trelud/energienutzung/Controller/YearController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/energienutzung/years")
+@RequestMapping("/energy/years")
 @RequiredArgsConstructor
 public class YearController {
     public final YearService yearService;
