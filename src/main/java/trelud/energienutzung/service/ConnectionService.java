11e974c25a1780e65525913ac558239d3f5d81a3
grujud22 <grujud22@htl-kaindorf.at>
2026-06-19 16:19:13 +0200

    started service implementation

--- src/main/java/trelud/energienutzung/service/ConnectionService.java
+++ src/main/java/trelud/energienutzung/service/ConnectionService.java
@@ -3,9 +3,15 @@ package trelud.energienutzung.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import trelud.energienutzung.database.*;
+import trelud.energienutzung.pojo.Fuel;
+import trelud.energienutzung.pojo.Region;
+import trelud.energienutzung.pojo.Sector;
+import trelud.energienutzung.pojo.Year;
 
+import java.rmi.NoSuchObjectException;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +23,19 @@ public class ConnectionService {
     private final RegionRepository regionRepository;
     private final YearRepository yearRepository;
 
-    public List<Map<String, Object>> getConnectionsByYearByRegionBySectorByFuelType(
+    public List<Map<String, Object>> getConnectionsByYearByRegionBySectorByFuelType (
             int yearNumber,
             String regionName,
             String sectorName,
             String fuelTypeName
-    ){
+    ) throws NoSuchObjectException {
+        Region region = regionRepository.getRegionByName(regionName);
+        Sector sector = sectorRepository.getSectorByName(sectorName);
+        Fuel fuel = fuelRepository.getFuelByName(fuelTypeName);
+
+
+
+
         return null;
     }
 
