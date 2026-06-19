0e32c8a94670e542761f33693599a81929a3ae82
grujud22 <grujud22@htl-kaindorf.at>
2026-06-19 11:08:18 +0200

    added query to find fuel by name

--- src/main/java/trelud/energienutzung/database/FuelRepository.java
+++ src/main/java/trelud/energienutzung/database/FuelRepository.java
@@ -1,9 +1,15 @@
 package trelud.energienutzung.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import trelud.energienutzung.pojo.Fuel;
 
 @Repository
 public interface FuelRepository extends JpaRepository<Fuel, Long> {
+    @Query("SELECT f FROM Fuel f WHERE f.fuelName = :fuelName")
+    Fuel getFuelByName(
+            @Param("fuelName") String fuelName
+    );
 }
